27149c514fabc45fb290869fb9a9fd84ed5d6f73
Joshua Santos <nerdsville@nerdsville.net>
2015-12-28 07:39:56 -0800

    setup media root

--- pgweb/settings/development.py
+++ pgweb/settings/development.py
@@ -1,6 +1,6 @@
 # Django settings for pgweb project.
 
-ALLOWED_HOSTS=['*'] 
+ALLOWED_HOSTS=['*']
 DEBUG = False
 #TEMPLATE_DEBUG = DEBUG
 
@@ -34,11 +34,11 @@ MEDIA_ROOT = ''
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 STATIC_URL = '/media/'
 
 STATICFILES_DIRS = (
-	'media',
+	'static',
 )
 
 # Make this unique, and don't share it with anybody.
