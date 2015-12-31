b53855f62ba18a63a9c05a6cc04772f14ea93db1
Joshua Santos <nerdsville@nerdsville.net>
2015-12-31 09:29:03 -0800

    Fix image path

--- pgweb/blog/models.py
+++ pgweb/blog/models.py
@@ -14,7 +14,7 @@ class BlogPost(models.Model):
     meta_description = models.TextField(blank=True, null=True)
     list_on_side = models.BooleanField(default=False)
     featured = models.BooleanField(default=False, help_text="Main featured blog post (we can only have one)")
-    feature_image = models.ImageField(default=None, blank=True, null=True, upload_to = 'media/images/featured_blog', help_text="Image to be displayed when the blog is featured on the home page")
+    feature_image = models.ImageField(default=None, blank=True, null=True, upload_to = 'images/featured_blog', help_text="Image to be displayed when the blog is featured on the home page")
 
     def __unicode__(self):
 	    return self.title
