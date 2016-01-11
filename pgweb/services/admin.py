650fc7c2ec349328a66c921f21641a045c4eb3d2
Joshua Santos <nerdsville@nerdsville.net>
2016-01-10 21:06:26 -0800

    Add Service types

--- pgweb/services/admin.py
+++ pgweb/services/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from pgweb.util.admin import PgwebAdmin
-from models import Service, PackageService
+from models import Service, ServiceSection, PackageService, ProfessionalService, CloudService
 
-admin.site.register(Service)
 admin.site.register(PackageService)
+admin.site.register(ProfessionalService)
+admin.site.register(CloudService)
+admin.site.register(Service)
+admin.site.register(ServiceSection)
