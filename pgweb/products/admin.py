2ac8d786120d1ec2c24fca1ef0e502c867477d1a
Joshua Santos <nerdsville@nerdsville.net>
2016-02-07 11:54:33 -0800

    ProductSection admin modification

--- pgweb/products/admin.py
+++ pgweb/products/admin.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from pgweb.util.admin import PgwebAdmin
 from models import Product, ProductSection, ProductFeatureGrid, ProductFeatureGridObject
 
+class ProductSectionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'product')
+
 admin.site.register(Product)
-admin.site.register(ProductSection)
+admin.site.register(ProductSection, ProductSectionAdmin)
 admin.site.register(ProductFeatureGrid)
 admin.site.register(ProductFeatureGridObject)
